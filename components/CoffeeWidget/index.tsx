feec38346533b5ecdd2313b0567b8ce97c053fc5
j620656786206@gmail.com <jQkUH)*AXyT,6@9B>
2025-05-03 21:51:11 +0800

    refactor: Update Buymeacoffee component

--- components/CoffeeWidget/index.tsx
+++ components/CoffeeWidget/index.tsx
@@ -1,35 +1,33 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React from 'react';
+import Script from 'next/script';
 
 const Buymeacoffee = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    const div = document.getElementById('supportByBMC');
-    script.setAttribute('data-name', 'BMC-Widget');
-    script.src = 'https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js';
-    script.setAttribute('data-id', 'j620656786206');
-    script.setAttribute('data-description', 'Support me on Buy me a coffee!');
-    script.setAttribute(
-      'data-message',
-      'Thank you for visiting my website. If this app has helped you in anyway, consider buying me a coffee. ✨😎'
-    );
-    script.setAttribute('data-color', '#ec4899');
-    script.setAttribute('data-position', 'Right');
-    script.setAttribute('data-x_margin', '18');
-    script.setAttribute('data-y_margin', '18');
-    script.async = true;
-    document.head.appendChild(script);
-    script.onload = function () {
-      const evt = document.createEvent('Event');
-      evt.initEvent('DOMContentLoaded', false, false);
-      window.dispatchEvent(evt);
-    };
-
-    div?.appendChild(script);
-  }, []);
-
-  return <div id="supportByBMC"></div>;
+  return (
+    <>
+      <div id="supportByBMC"></div>
+      <Script
+        id="bmc-widget"
+        src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+        strategy="lazyOnload"
+        data-name="BMC-Widget"
+        data-id="j620656786206"
+        data-description="Support me on Buy me a coffee!"
+        data-message="Thank you for visiting my website. If this app has helped you in anyway, consider buying me a coffee. ✨😎"
+        data-color="#ec4899"
+        data-position="Right"
+        data-x_margin="18"
+        data-y_margin="18"
+        onLoad={() => {
+          // 觸發 BMC widget 初始化
+          const evt = document.createEvent('Event');
+          evt.initEvent('DOMContentLoaded', false, false);
+          window.dispatchEvent(evt);
+        }}
+      />
+    </>
+  );
 };
 
 export default Buymeacoffee;
